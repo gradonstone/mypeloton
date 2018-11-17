5751481b0f16fce713bf53166121ed52bcf1fc6f
Nicholas Angle <nja14@my.fsu.edu>
2018-11-17 11:10:47 -0500

    Added a Basic Query
    Basic query added (only validates by similar times!). Working on adding more validations. It stores user requests into a list of requests, so the list section of the request activity should be doable now

--- app/src/main/java/edu/fsu/cs/mobile/mypeloton/Request.java
+++ app/src/main/java/edu/fsu/cs/mobile/mypeloton/Request.java
@@ -10,6 +10,8 @@ public class Request {
     //1 for active request, 0 for inactive request
     public int active;
 
+    public Request(){}
+
     public Request(String userID, String email, String ride_type, int distance, int time, int active)
     {
         this.userID = userID;
@@ -19,4 +21,23 @@ public class Request {
         this.time = time;
         this.active = active;
     }
+
+    public String getUserID()
+    { return userID; }
+
+    public String getEmail()
+    { return email; }
+
+    public String getRide_type()
+    { return ride_type; }
+
+    public int getDistance()
+    { return distance; }
+
+    public int getTime()
+    { return time; }
+
+    public int getActive()
+    { return active; }
+
 }
--- app/src/main/java/edu/fsu/cs/mobile/mypeloton/RequestActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/mypeloton/RequestActivity.java
@@ -8,6 +8,7 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -17,10 +18,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 //Code inspired by lecture9examples>locationexaples
@@ -34,6 +39,7 @@ public class RequestActivity extends AppCompatActivity implements ActivityCompat
     private DatabaseReference mDatabase;
     private Button cancelRequest;
     private String uid;
+    private List<Request> requestList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +49,33 @@ public class RequestActivity extends AppCompatActivity implements ActivityCompat
         uid = getIntent().getExtras().getString("uid");
         cancelRequest = (Button) findViewById(R.id.cancel_request_button);
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        int time = 5;
+
+        requestList = new ArrayList<>();
+        mDatabase.child("requests").orderByChild("time").equalTo(time)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        requestList.clear();
+                        if(dataSnapshot.exists()){
+                            for(DataSnapshot requestSnapshot : dataSnapshot.getChildren()){
+                                String userID = requestSnapshot.child("userID").getValue(String.class);
+                                String email = requestSnapshot.child("email").getValue(String.class);
+                                String rideType = requestSnapshot.child("ride_type").getValue(String.class);
+                                int time = requestSnapshot.child("time").getValue(Integer.class);
+                                int distance = requestSnapshot.child("distance").getValue(Integer.class);
+                                int active = requestSnapshot.child("active").getValue(Integer.class);
+                                Request request = new Request(userID, email, rideType, distance, time, active);
+                                requestList.add(request);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Toast.makeText(RequestActivity.this, "Database read failed", Toast.LENGTH_LONG).show();
+                    }
+                });
 
         cancelRequest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,8 +87,8 @@ public class RequestActivity extends AppCompatActivity implements ActivityCompat
             }
         });
 
-        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        requestLocationUpdates();
+        //mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        //requestLocationUpdates();
 
         // first, get all data from database
         // create service to check database every 30 seconds or so
