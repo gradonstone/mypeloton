b220d7e145d3fb98f9aaac9b09b5db1ab8fd9fbe
Hung-Tan-Tran <HTT15@my.fsu.edu>
2018-11-18 08:58:28 -0500

    GPS revised with GooglePlayService, with best practices

--- app/src/main/java/edu/fsu/cs/mobile/mypeloton/RequestActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/mypeloton/RequestActivity.java
@@ -3,21 +3,30 @@ package edu.fsu.cs.mobile.mypeloton;
 import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,24 +36,37 @@ import com.google.firebase.database.ValueEventListener;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
-//Code inspired by lecture9examples>locationexaples
+//Code inspired by GoogleSamples -> Android Play Location
 
 public class RequestActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
-    private static final int PERMISSION_REQUEST_LOCATIONS = 3;
-    private TextView mTextStatus;
-    private LocationManager mLocationManager;
-    private boolean mLocationPermissionGranted = false;
     private DatabaseReference mDatabase;
     private Button cancelRequest;
     private String uid;
     private List<Request> requestList;
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+    private FusedLocationProviderClient mFusedLocationClient;
+    protected Location mLastLocation;
+    private String mLatitudeLabel;
+    private String mLongitudeLabel;
+    private TextView mLatitudeText;
+    private TextView mLongitudeText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
+
+        mLatitudeLabel = getResources().getString(R.string.latitude_label);
+        mLongitudeLabel = getResources().getString(R.string.longitude_label);
+        mLatitudeText = (TextView) findViewById((R.id.latitude_text));
+        mLongitudeText = (TextView) findViewById((R.id.longitude_text));
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         uid = getIntent().getExtras().getString("uid");
         cancelRequest = (Button) findViewById(R.id.cancel_request_button);
@@ -88,104 +110,119 @@ public class RequestActivity extends AppCompatActivity implements ActivityCompat
                 startActivity(myIntent);
             }
         });
-
-        //mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        //requestLocationUpdates();
-
-        // first, get all data from database
-        // create service to check database every 30 seconds or so
-        // list adapter to show all matches, otherwise, show the text field searching...
-
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        //showLastKnownLocation();
+    public void onStart() {
+        super.onStart();
+
+        if (!checkPermissions()) {
+            requestPermissions();
+        } else {
+            getLastLocation();
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void getLastLocation() {
+        mFusedLocationClient.getLastLocation()
+                .addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            mLastLocation = task.getResult();
+                            //TO-DO: Push latitude and longitude into database
+                            mLatitudeText.setText(String.format(Locale.ENGLISH, "%s: %f",
+                                    mLatitudeLabel,
+                                    mLastLocation.getLatitude()));
+                            mLongitudeText.setText(String.format(Locale.ENGLISH, "%s: %f",
+                                    mLongitudeLabel,
+                                    mLastLocation.getLongitude()));
+                        } else {
+                            Log.w(TAG, "getLastLocation:exception", task.getException());
+                            showSnackbar(getString(R.string.no_location_detected));
+                        }
+                    }
+                });
+    }
+
+    private void showSnackbar(final String text) {
+        View container = findViewById(R.id.activity_request_container);
+        if (container != null) {
+            Snackbar.make(container, text, Snackbar.LENGTH_LONG).show();
+        }
+    }
+
+    private void showSnackbar(final int mainTextStringId, final int actionStringId,
+                              View.OnClickListener listener) {
+        Snackbar.make(findViewById(android.R.id.content),
+                getString(mainTextStringId),
+                Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(actionStringId), listener).show();
+    }
+
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(RequestActivity.this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_PERMISSIONS_REQUEST_CODE);
+    }
+
+    private void requestPermissions() {
+        boolean shouldProvideRationale =
+                ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        if (shouldProvideRationale) {
+            Log.i(TAG, "Displaying permission rationale to provide additional context.");
+
+            showSnackbar(R.string.permission_rationale, android.R.string.ok,
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            // Request permission
+                            startLocationPermissionRequest();
+                        }
+                    });
+
+        } else {
+            Log.i(TAG, "Requesting permission");
+            startLocationPermissionRequest();
+        }
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        //mLocationManager.removeUpdates(mLocationListener);
-    }
-
-    public void requestLocationUpdates(){
-        Toast.makeText(getApplicationContext(), "Waiting for location ...",
-                Toast.LENGTH_SHORT).show();
-        if(!mLocationPermissionGranted) {
-            requestLocationsPermission();
-        }
-        try {
-            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocationListener);
-        } catch (SecurityException e) {
-            requestLocationsPermission();
-        }
-    }
-
-    private void requestLocationsPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(RequestActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(RequestActivity.this);
-            builder.setTitle("Requesting internet permissions");
-            builder.setMessage("This application requires internet. Accept to continue");
-            builder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialogInterface, int i) {
-                    ActivityCompat.requestPermissions(RequestActivity.this,
-                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                                    Manifest.permission.ACCESS_COARSE_LOCATION},
-                            PERMISSION_REQUEST_LOCATIONS);
-                }
-            });
-            builder.show();
-        }
-        else {
-            ActivityCompat.requestPermissions(RequestActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION},
-                    PERMISSION_REQUEST_LOCATIONS);
-        }
-
-    }
-
-
-    private LocationListener mLocationListener = new LocationListener() {
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-            Toast.makeText(getApplicationContext(), "Waiting for location",
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            Toast.makeText(getApplicationContext(), "Connection Lost",
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onLocationChanged(Location location) {
-            Geocoder geo = new Geocoder(getApplicationContext());
-            List<Address> addresses = null;
-
-            try {
-                addresses = geo.getFromLocation(location.getLatitude(),
-                        location.getLongitude(), 10);
-            } catch (IOException e) {
-                e.printStackTrace();
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        Log.i(TAG, "onRequestPermissionResult");
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.length <= 0) {
+                Log.i(TAG, "User interaction was cancelled.");
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation();
+            } else {
+                showSnackbar(R.string.permission_denied_explanation, R.string.settings,
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Intent intent = new Intent();
+                                intent.setAction(
+                                        Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package",
+                                        BuildConfig.APPLICATION_ID, null);
+                                intent.setData(uri);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        });
             }
-
-            if (addresses == null) {
-                return;
-            }
-            if (addresses.size() > 0 &&
-                    addresses.get(0).getMaxAddressLineIndex() >= 0)
-                mTextStatus.setText(addresses.get(0).getAddressLine(0));
         }
-    };
-
+    }
 }
+
+
